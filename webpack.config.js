c097d660f01349613f4d9ca1e49863ec5caf67fb
Oksana Firtych <oxana.firtych@gmail.com>
2019-10-28 10:16:53 +0300

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
     entry: {
         server: './src/server.js',
@@ -35,12 +34,5 @@ module.exports = {
                 use: [{ loader: "html-loader" }]
             }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/test.html",
-            filename: "./test.html",
-            excludeChunks: ['server']
-        })
-    ]
+    }
 }
